62af1f5e32b7a579cd95c11b19d9b21d774e8335
kamimi <lywbh@qq.com>
2019-08-28 13:37:05 +0800

    HashMap→ConcurrentHashMap

--- src/main/java/com/lyw/config/GameStatus.java
+++ src/main/java/com/lyw/config/GameStatus.java
@@ -2,12 +2,12 @@ package com.lyw.config;
 
 import com.lyw.core.IdiomGame;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class GameStatus {
 
-    private static Map<Long, IdiomGame> GAME_MAP = new HashMap<>();
+    private static Map<Long, IdiomGame> GAME_MAP = new ConcurrentHashMap<>();
 
     public static boolean isRunning(long groupId) {
         return GAME_MAP.containsKey(groupId);
